705937b1f7b537784e59f112aa0d29486ad3cbb2
amehta2025 <116235673+amehta2025@users.noreply.github.com>
2026-08-06 18:54:27 -0500

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,18 @@
+#include <map>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> m;
+        for (int i = 0; i < nums.size(); ++i) {
+            m[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            int difference = target - nums[i];
+            if (m.count(difference) && m[difference] != i)  {
+                return {i, m[difference]};
+            }
+        }
+    }
+};
