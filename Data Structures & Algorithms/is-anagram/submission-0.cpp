2a316c91806de9d7cce5941fc6094418657da4a3
amehta2025 <116235673+amehta2025@users.noreply.github.com>
2026-08-06 18:15:33 -0500

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,18 @@
+#include <map>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> one;
+        map<char, int> two;
+        if (s.size() != t.size()) {
+            return false;
+        }
+        for (int i = 0; i < s.size(); i++) {
+            one[s[i]]++;
+            two[t[i]]++;
+        }
+        if (one == two) return true;
+        else return false;
+    }
+};
