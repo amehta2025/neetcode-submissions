8d7fe5b2776ad453c6a865a357ec412a8fe48c69
amehta2025 <116235673+amehta2025@users.noreply.github.com>
2026-08-11 14:52:45 -0500

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i = 0;
+        int j = nums.size() - 1;
+        while (i <= j) {
+            int middle = (i + j) /2;
+            if (target < nums[middle]) j = middle - 1;
+            else if (target > nums[middle]) i = middle + 1;
+            else return middle;
+        }
+        return -1;
+    }
+};
