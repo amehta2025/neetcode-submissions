00543c79296d667162aeaa79df6a7ce6e67a9db1
Anish Mehta <116235673+amehta2025@users.noreply.github.com>
2026-08-16 18:23:48 -0500

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1,1
+        for i in range(n-1):
+            two, one = one, two + one
+        
+        return one
+
+
+
+#dynamic programming is solving complex problems by breaking them down into simpler problems
+#top down (memoization) --> break a problem down recursively
+#bottom up (tabulation) --> starts from base cases and works its way to final solution
+# (caching) means storing the result so you can use it next time instead of calculating the same thing again and again
+#store memory of smaller subproblems in dp
